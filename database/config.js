b84f8edfdf9a2a14c10ee1f545f47df07a893913
Adan Gimenez <adangimenez@Adans-MBP.attlocal.net>
2022-04-02 14:53:32 -0500

    firts commit

---
+++ database/config.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+    try {
+      await mongoose.connect(process.env.MONGODB_CNN, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      });  
+
+    console.log('db online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('error al levantar db')
+    }
+}
+
+
+module.exports = {
+    dbConnection
+}
---
+++ routes/usuarios.js
@@ -0,0 +1,51 @@
+
+const { Router } = require('express');
+const {check} = require('express-validator');
+
+
+const {validarCampos} = require('../middlewares/validar-campos');
+const {esRoleValido, emailExiste, existeUsuarioPorId} = require('../helpers/db-validators');
+
+const { usuariosGet,
+        usuariosPut,
+        usuariosPost,
+        usuariosDelete,
+        usuariosPatch } = require('../controllers/usuarios');
+
+
+const router = Router();
+
+
+router.get('/', usuariosGet );
+
+router.put('/:id', [
+    check('id', 'No es un id').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos    
+], usuariosPut );
+
+router.post('/', [
+    check('nombre', 'el nombre es obligatorio').not().isEmpty(),
+    check('password', 'el password debe ser mas de 6 letras').isLength({min: 6}),
+    check('correo', 'correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
+    //check('rol', 'no es un rol valido').isIn('ADMIN_ROLE','USER_ROLE'),
+    check('rol').custom(esRoleValido),
+    validarCampos
+],usuariosPost );
+
+
+router.delete('/:id',[
+    check('id', 'No es un id').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosDelete );
+
+router.patch('/', usuariosPatch );
+
+
+
+
+
+module.exports = router;
